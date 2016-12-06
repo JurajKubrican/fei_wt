bd4eee401d211f89dd0554b8461c4704779359dc
Johnny <sedivy.jan.j@gmail.com>
2016-12-06 16:09:34 +0100

    Pridal som pamiatky
    pridal som obrazky a aj popis do pamiatky_generator.php

--- pamiatky_generator.php
+++ pamiatky_generator.php
@@ -18,6 +18,22 @@ $pamiatky = [
     'opis' => 'Solivar v Prešove, národná kultúrna pamiatka patrí medzi najvýznamnejšie technické pamiatky na Slovensku. Je to unikátny komplex technických objektov na čerpanie a varenie soli zo soľanky, pochádzajúci zo 17. storočia. K objektom na ťažbu kamennej soli a čerpanie soľanky v Solivare patrí najmä šachta Leopold, četerne (rezervoáre soľanky), huta , varňa , sklad soli (komory), kováčske dielne a klopačka (turňa).',
   ],
   (object)[
+    'nazov'=> 'Hvezdáreň Skalnaté Pleso',
+    'rokVzniku' => 1940,
+    'sirka' => '49.189347',
+    'dlzka' => '20.2341771',
+    'img' => 'img/Observatorium_Skalnate_pleso.jpg',
+    'opis' => 'Astronomické observatórium Skalnaté Pleso sa nachádza neďaleko Skalnatého plesa v nadmorskej výške 1 786 nad hladinou mora. Observatórium bolo založené v roku 1943, keď boli vykonané prvé pozorovania. V súčasnosti má hvezdáreň dva ďalekohľady. V malej kupole sa nachádza 61 cm (f/4,2) automatický ďalekohlad typu Newton , ktorý je vybavený citlivou CCD kamerou a sadou širokopásmových filtrov na fotometrické pozorovanie planétok a komét. Vo veľkej kupole observatória je 1,3m ďalekohľad typu Nasmyth-Cassegrain . V Nasmythových ohniskách sú k dispozícii viaceré postfokusové zariadenia (vysokodisperzný échelle spektrograf, veľkoformátové vizuálne CCD kamery a infračervená CCD kamera). Ďalekohľad je v súčasnosti v testovacej prevádzke a v skorej dobe bude využitý na pozorovanie objektov Slnečnej sústavy (kométy, asteroidy) ale aj stelárnych objektov (premenné hviezdy, materské hviezdy exoplanét). Pre obidva ďalekohľady slúži moderné pokovovacie zariadenie, ktoré umožňuje nanášať tenké odrazné vrstvy na zrkadlá vo vysokom vákuu.',
+  ],
+  (object)[
+    'nazov'=> 'Mlyn Jána Maticu',
+    'rokVzniku' => 1893,
+    'sirka' => '48.0887117',
+    'dlzka' => '17.6663053',
+    'img' => 'img/Mlyn_jana_maticu.jpg',
+    'opis' => 'Mlyn reprezentuje typ tzv. Pobrežného kolového mlyna. Postavený je na pilótach z agátového dreva v toku rieky na ostrohe, vytvorenej Malým Dunajom a jeho ľavobrežným prítokom Suchým potokom. Budova mlyna rámovej konštrukcie s doštenou výplňou je z brehu prístupná drevenou lávkou so zábradlím. Veľké lopatkové dvojvencové hnacie koleso na spodnú vodu je v prúde rieky upevnené na podpornej stolici s doštenou plošinou. Dominantným objektom mlyna je drevená mlynica na pilótoch, so sedlovou strechou krovovej hambálkovej konštrukcie, krytá šindľom. Zaujímavosťou je štrbinové okienko, ktorým mlynár pozoroval chod náhonového kolesa, ako aj príchod gazdov s melivom z druhého brehu rieky. Dvojvencové lopatkové koleso s drevenými ramenami má priemer 7 m, široké je 2,5m. Pobrežný kolový mlyn v Tomášikove postavil v r. 1893 mlynár Ján Matica, starý otec posledného majiteľa rovnakého mena (narodený r. 1920). Mlyn sa zachoval v pôvodnom stave, bez rušivých zásahov do konštrukcie, s uceleným prevádzkyschopným mlynským zariadením. V mlyne sa mlelo až do r. 1960. V r. 1982 bol mlyn obnovený a zariadený ako expozícia Vlastivedného múzea v Galante.',
+  ],
+  (object)[
     'nazov'=> 'pamiatka3',
     'rokVzniku' => 1995,
     'sirka' => '123',
