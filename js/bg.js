77d3e56bcb3403f4045d8c4452436dfed4520202
Juraj Kubričan <kubrican.juraj@gmail.com>
2016-12-08 17:00:32 +0100

    img bg

--- js/bg.js
+++ js/bg.js
@@ -1,32 +1,44 @@
 (function($){
   ('use strict');
 
-  var colors = [
-    'rgba(157, 59, 198, 0.5)',
-    'rgba(255, 82, 100, 0.5)',
-    'rgba(255, 98, 42, 0.5)',
-    'rgba(255, 216, 67, 0.5)',
-    'rgba(0, 169, 191, 0.5)',
-    'rgba(136, 205, 78, 0.5)',
-    'rgba(127, 141, 144, 0.5)',
-    'rgba(60, 128, 228, 0.5)',
-  ]
+  // var colors = [
+  //   'rgba(157, 59, 198, 0.5)',
+  //   'rgba(255, 82, 100, 0.5)',
+  //   'rgba(255, 98, 42, 0.5)',
+  //   'rgba(255, 216, 67, 0.5)',
+  //   'rgba(0, 169, 191, 0.5)',
+  //   'rgba(136, 205, 78, 0.5)',
+  //   'rgba(127, 141, 144, 0.5)',
+  //   'rgba(60, 128, 228, 0.5)',
+  // ]
 
   $(document).ready(function(){
     $('body>.container').prepend($('<ul>',{id:'parallax'}));
-    for(var i = 0; i<20; i++){
-      $('#parallax').append($('<li>',{class:'layer','data-depth' : Math.random()}));
-    }
+    loadPamiatky('js/pamiatky.json');
+  })
+
+  function makeParallax(){
     $('#parallax > li').each(function(){
       var size = (50 + (1 - $(this).data('depth')) * 250)+'px'
-      $(this).css('background-color',colors[Math.floor($(this).data('depth') * 8)])
-      $(this).css('width',size)
-      $(this).css('height',size)
-      $(this).css('margin-top',(Math.random() * 100 - 5)+'vh')
-      $(this).css('margin-left',(Math.random() * 100 - 5)+'vw')
+      $(this).css('margin-top',(Math.random() * 60 + 15)+'vh')
+      $(this).css('margin-left',(Math.random() * 60 + 15)+'vw')
     })
     $('#parallax').parallax();
     $('#parallax').show();
     $('body:eq(0)').remove();
-  })
+  }
+
+
+  function loadPamiatky(url){
+    $.get(url,function(data){
+      dataJSON = data;
+      for(var i in data){
+        var img = $('<img>',{src:data[i].img,alt:data[i].meno});
+        var li = $('<li>',{class:'layer','data-depth' : Math.random()});
+        li.append(img);
+        $('#parallax').append(li);
+      }
+      makeParallax();
+    });
+  }
 }(jQuery))
