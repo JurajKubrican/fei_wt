19e6762f40537ba71f8d0496dc012e3c52c10021
Juraj Kubričan <kubrican.juraj@gmail.com>
2016-12-10 18:59:36 +0100

    homepage

--- js/bg.js
+++ js/bg.js
@@ -1,17 +1,6 @@
 (function($){
   ('use strict');
 
-  // var colors = [
-  //   'rgba(157, 59, 198, 0.5)',
-  //   'rgba(255, 82, 100, 0.5)',
-  //   'rgba(255, 98, 42, 0.5)',
-  //   'rgba(255, 216, 67, 0.5)',
-  //   'rgba(0, 169, 191, 0.5)',
-  //   'rgba(136, 205, 78, 0.5)',
-  //   'rgba(127, 141, 144, 0.5)',
-  //   'rgba(60, 128, 228, 0.5)',
-  // ]
-
   $(document).ready(function(){
     $('body>.container').prepend($('<ul>',{id:'parallax'}));
     loadPamiatky('js/pamiatky.json');
@@ -21,9 +10,6 @@
   })
 
   function makeParallax(){
-    $('#parallax > li').each(function(){
-      var size = (50 + (1 - $(this).data('depth')) * 250)+'px'
-    })
     $('#parallax').parallax();
     $('#parallax').show();
     $('body:eq(0)').remove();
@@ -34,9 +20,11 @@
     $.get(url,function(data){
       data = typeof(data) === 'string' ? JSON.parse(data):data ;
       for(var i in data){
-        var li = $('<li>',{class:'layer','data-depth' : Math.random()});
-        li.append($('<h1>' + data[i].nazov + '</h1>'));
-        li.append($('<img>',{src:data[i].img,alt:data[i].nazov}));
+        var li = $('<li>',{class:'layer','data-depth' : (Math.random() / 3 + 0.0)});
+        var a = $('<a>',{href:'pamiatky.html#bgimg' + i });
+        li.append(a);
+        a.append($('<h1>' + data[i].nazov + '</h1>'));
+        a.append($('<img>',{src:data[i].img,alt:data[i].nazov}));
         $('#parallax').append(li);
       }
       makeParallax();
